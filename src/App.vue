f6cd28eac1bc2055ec62136fd6c956aeacb9b61a
YEslahi <yasi.e72@gmail.com>
2021-06-05 22:03:33 +0200

    supermarket frontend get product

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    {{ products }}
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      products: [],
+    }
+  },
+  methods: {
+    getProducts() {
+      const url = 'http://127.0.0.1:8000/api/v1/';
+      fetch(url)
+        .then(response => response.json())
+        .then(data => this.products = data);
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
